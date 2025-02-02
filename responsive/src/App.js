b7d97156664e3546499ba01af127eb2c9f591df3
Barbora6 <smetanovabarbora@email.cz>
2025-02-02 19:53:47 +0100

    nová componenta Buttons

--- responsive/src/App.js
+++ responsive/src/App.js
@@ -1,19 +1,11 @@
-import { Button } from "@mui/material";
 import React from "react";
+import Buttons from "./components/Buttons";
 
 const App = () => {
   return (
     <div>
       <h2>neco</h2>
-      <Button variant="text" color="primary" size="large">
-        Text
-      </Button>
-      <Button variant="contained" color="secondary" size="medium">
-        Contained
-      </Button>
-      <Button variant="outlined" color="primary" size="small">
-        Outlined
-      </Button>
+      <Buttons />
     </div>
   );
 };
---
+++ responsive/src/components/Buttons.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@mui/material";
+import React from "react";
+
+const Buttons = () => {
+  return (
+    <div>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+    </div>
+  );
+};
+
+export default Buttons;
