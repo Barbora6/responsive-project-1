eb8350ccd323d20b86eca538fd83bed773b3473c
Barbora6 <smetanovabarbora@email.cz>
2025-02-02 19:35:20 +0100

    buttons

--- responsive/src/App.js
+++ responsive/src/App.js
@@ -1,7 +1,21 @@
+import { Button } from "@mui/material";
 import React from "react";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h2>neco</h2>
+      <Button variant="text" color="primary" size="large">
+        Text
+      </Button>
+      <Button variant="contained" color="secondary" size="medium">
+        Contained
+      </Button>
+      <Button variant="outlined" color="primary" size="small">
+        Outlined
+      </Button>
+    </div>
+  );
 };
 
 export default App;
