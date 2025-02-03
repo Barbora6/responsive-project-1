6623ce89d56500b0eb1a5a5dae007ab8e2e595d3
Barbora6 <smetanovabarbora@email.cz>
2025-02-03 15:10:54 +0100

    přidání buttnů

--- responsive/src/App.js
+++ responsive/src/App.js
@@ -4,7 +4,6 @@ import Buttons from "./components/Buttons";
 const App = () => {
   return (
     <div>
-      <h2>neco</h2>
       <Buttons />
     </div>
   );
--- responsive/src/components/Buttons.jsx
+++ responsive/src/components/Buttons.jsx
@@ -1,12 +1,42 @@
-import { Button } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import React from "react";
+import { IoMdSettings } from "react-icons/io";
+import { FaPlus } from "react-icons/fa";
 
 const Buttons = () => {
   return (
     <div>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
+      <Button variant="text" color="primary" size="medium">
+        TEXT
+      </Button>
+      <Button
+        variant="contained"
+        color="secondary"
+        size="large"
+        startIcon={<IoMdSettings />}
+      >
+        Text
+      </Button>
+      <Button
+        variant="contained"
+        color="primary"
+        size="medium"
+        startIcon={<FaPlus />}
+      >
+        Add new post
+      </Button>
+      <Button variant="outlined" color="secondary" size="small">
+        Outlined
+      </Button>
+      <Typography variant="h1" component="p">
+        Nadpis h1, ale užívá paragraph.
+      </Typography>
+      <Button
+        variant="contained"
+        sx={{ backgroundColor: "skyblue", color: "#888", margin: 5 }}
+      >
+        Můj unikátní tlačítko.
+      </Button>
     </div>
   );
 };
