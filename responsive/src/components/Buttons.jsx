62d97a5a730e38fea7049b9288aa56104831a2c7
Barbora6 <smetanovabarbora@email.cz>
2025-02-03 15:52:00 +0100

    theme

--- responsive/src/components/Buttons.jsx
+++ responsive/src/components/Buttons.jsx
@@ -1,9 +1,35 @@
-import { Button, Typography } from "@mui/material";
+import { Button, styled, Typography } from "@mui/material";
 import React from "react";
 import { IoMdSettings } from "react-icons/io";
 import { FaPlus } from "react-icons/fa";
 
 const Buttons = () => {
+  const BlueButton = styled(Button)({
+    backgroundColor: "darkblue",
+    color: "white",
+    margin: 5,
+    "&:hover": {
+      backgroundColor: "lightblue"
+    },
+    "&:disabled": {
+      backgroundColor: "gray",
+      color: "white"
+    }
+  });
+
+  const RedButton = styled(Button)({
+    backgroundColor: "red",
+    color: "white",
+    margin: 5,
+    "&:hover": {
+      backgroundColor: "lightblue"
+    },
+    "&:disabled": {
+      backgroundColor: "gray",
+      color: "white"
+    }
+  });
+
   return (
     <div>
       <Button variant="text" color="primary" size="medium">
@@ -33,10 +59,40 @@ const Buttons = () => {
       </Typography>
       <Button
         variant="contained"
-        sx={{ backgroundColor: "skyblue", color: "#888", margin: 5 }}
+        sx={{
+          backgroundColor: "darkblue",
+          color: "white",
+          margin: 5,
+          "&:hover": {
+            backgroundColor: "lightblue"
+          },
+          "&:disabled": {
+            backgroundColor: "gray",
+            color: "white"
+          }
+        }}
       >
         Můj unikátní tlačítko.
       </Button>
+      <Button
+        variant="contained"
+        sx={{
+          backgroundColor: "red",
+          color: "white",
+          margin: 5,
+          "&:hover": {
+            backgroundColor: "lightblue"
+          },
+          "&:disabled": {
+            backgroundColor: "gray",
+            color: "white"
+          }
+        }}
+      >
+        Můj unikátní tlačítko.
+      </Button>
+      <BlueButton>Modré tlačítko</BlueButton>
+      <RedButton>Červené tlačítko</RedButton>
     </div>
   );
 };
