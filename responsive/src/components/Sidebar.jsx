4eb7870c6d72fa0379c789012e3dae8e9a2766e1
Barbora6 <smetanovabarbora@email.cz>
2025-02-06 11:26:07 +0100

    icons for Sidebar

--- responsive/src/components/Sidebar.jsx
+++ responsive/src/components/Sidebar.jsx
@@ -1,9 +1,24 @@
-import { Box } from "@mui/material";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText
+} from "@mui/material";
+
+import { IoHomeSharp } from "react-icons/io5";
+import { IoMdSettings } from "react-icons/io";
+import { IoPerson } from "react-icons/io5";
+import { CgProfile } from "react-icons/cg";
+import { FaStore } from "react-icons/fa";
+import { MdGroups } from "react-icons/md";
+import { CgWebsite } from "react-icons/cg";
 
 export const Sidebar = () => {
   return (
     <Box
-      bgcolor="yellow"
+      bgcolor="lightgrey"
       flex={1}
       p={2}
       sx={{
@@ -16,7 +31,65 @@ export const Sidebar = () => {
         }
       }}
     >
-      Sidebar
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#home">
+            <ListItemIcon>
+              <IoHomeSharp size={28} />
+            </ListItemIcon>
+            <ListItemText primary="Homepage" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list">
+            <ListItemIcon>
+              <CgWebsite size={28} />
+            </ListItemIcon>
+            <ListItemText primary="Pages" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list">
+            <ListItemIcon>
+              <MdGroups size={28} />
+            </ListItemIcon>
+            <ListItemText primary="Groups" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list">
+            <ListItemIcon>
+              <FaStore size={28} />
+            </ListItemIcon>
+            <ListItemText primary="Market Place" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list">
+            <ListItemIcon>
+              <IoPerson size={28} />
+            </ListItemIcon>
+            <ListItemText primary="Fiends" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list">
+            <ListItemIcon>
+              <IoMdSettings size={28} />
+            </ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#simple-list">
+            <ListItemIcon>
+              <CgProfile size={28} />
+            </ListItemIcon>
+            <ListItemText primary="Profile" />
+          </ListItemButton>
+        </ListItem>
+      </List>
     </Box>
   );
 };
